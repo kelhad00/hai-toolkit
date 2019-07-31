237606b24e8e2c6d6593f3dc3c15c337a25cf8cd
noetits <noe.tits@gmail.com>
2019-07-31 12:51:17 +0200

    another TTS, it is really bad quality but works locally and fast

--- cozmo4resto/toolbox/tts_example.py
+++ cozmo4resto/toolbox/tts_example.py
@@ -5,6 +5,10 @@ import time
 gtts('Hello world, how are you doing?')
 time.sleep(3)
 
-
 tts_ibm('Hello world, how are you doing?')
 time.sleep(3)
+
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()
